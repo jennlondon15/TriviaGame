a667e7e51177f2ec782ec8055852f631b479df3b
jennlondon15 <jennlondon15@gmail.com>
2019-04-11 21:19:16 -0600

    js finitoooooo!!!!!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,23 @@
-// TODO Link images
-// TODO Make it looks awesome
+// TODO Display answer options in column
 // TODO Add a "next question" button when answer screen shows up
 
 const panel = $('.quiz');
 const countStartNumber = 30;
 
-// click events
-$(document).on('click', '#start-over', function(e) {
+// click event: play again button
+$(document).on('click', '#play-again', function(e) {
   game.reset();
 });
 
+// click event: answer selected
 $(document).on('click', '.answer-button', function(e) {
   game.clicked(e);
 });
 
+// click event: start button
 $(document).on('click', '.start', function(e) {
   $('.timer').prepend(
-    '<h2>Time Remaining: <span id="count-number">30</span> Seconds</h2>'
+    '<h2>Time Remaining: <span id="timed-number">30</span> Seconds</h2>',
   );
   game.loadQuestion();
 });
@@ -27,14 +28,14 @@ const questions = [
     question: 'How much were admission prices when Disney World first opened?',
     answers: ['$1.10', '$1.00', '$0.75', '$1.50'],
     correctAnswer: '$1.00',
-    image: '../images/question1.jpg',
+    image: './assets/images/question1.jpg',
   },
   {
     question:
       'When Disneyland was first built, what occupied the land before it was bought by Walt?',
     answers: ['A horse track', 'Open land', 'An apple farm', 'An orange grove'],
     correctAnswer: 'An orange grove',
-    image: '../images/question2.jpg',
+    image: './assets/images/question2.jpg',
   },
   {
     question:
@@ -46,7 +47,7 @@ const questions = [
       'A house near the park',
     ],
     correctAnswer: 'Above the firehouse',
-    image: '../images/question3.jpg',
+    image: './assets/images/question3.jpg',
   },
   {
     question:
@@ -59,13 +60,13 @@ const questions = [
     ],
     correctAnswer:
       'It is the speech Walt Disney gave when the park first opened',
-    image: '../images/question4.jpg',
+    image: './assets/images/question4.jpg',
   },
   {
     question: 'What was Mickey Mouse originally named?',
     answers: ['Elias Mouse', 'Oswald', 'Nicolas Mouse', 'Mortimer Mouse'],
     correctAnswer: 'Mortimer Mouse',
-    image: '../images/question5.jpg',
+    image: './assets/images/question5.jpg',
   },
   {
     question: 'Which Disneyland attraction was present opening day?',
@@ -76,20 +77,20 @@ const questions = [
       'The Enchanted Tiki Room',
     ],
     correctAnswer: 'The Jungle Cruise',
-    image: '../images/question6.jpg',
+    image: './assets/images/question6.jpg',
   },
   {
     question:
       'What is the name of the exclusive, invite-only car on the Disneyland Railroad?',
     answers: ['Lily Bell', 'C.K. Holliday', 'Ron Disney', 'Minnie Mouse'],
     correctAnswer: 'Lily Bell',
-    image: '../images/question7.jpg',
+    image: './assets/images/question7.jpg',
   },
   {
     question: 'How many days did it take to build Disneyland?',
     answers: ['1251', '421', '365', '999'],
     correctAnswer: '365',
-    image: '../images/question8.jpg',
+    image: './assets/images/question8.jpg',
   },
   {
     question:
@@ -101,7 +102,7 @@ const questions = [
       'Indiana Jones Adventure: Temple of the Forbidden Eye',
     ],
     correctAnswer: 'Radiator Springs Racers',
-    image: '../images/question9.jpg',
+    image: './assets/images/question9.jpg',
   },
   {
     question:
@@ -113,21 +114,25 @@ const questions = [
       'Peter Pans Flight',
     ],
     correctAnswer: 'The Pirates of the Caribbean',
-    image: '../images/question10.jpeg',
+    image: './assets/images/question10.jpeg',
   },
 ];
 
+// game play
 const game = {
   questions,
   currentQuestion: 0,
   count: countStartNumber,
   correct: 0,
   incorrect: 0,
+
+  // timer count
   countdown() {
     game.count--;
-    $('#count-number').html(game.count);
+    $('#timed-number').html(game.count);
     // console.log('#count-number');
 
+    // timer: time up
     if (game.count === 0) {
       // console.log('Looks Like You Ran Out Of Time!');
       game.timeUp();
@@ -140,23 +145,28 @@ const game = {
       panel.append(
         `${'<button class="answer-button" class="button"' + 'data-name="'}${
           questions[this.currentQuestion].answers[i]
-        }">${questions[this.currentQuestion].answers[i]}</button>`
+        }">${questions[this.currentQuestion].answers[i]}</button>`,
       );
     }
   },
+
+  // calling next question
   nextQuestion() {
     game.count = countStartNumber;
-    $('#count-number').html(game.count);
+    $('#timed-number').html(game.count);
     game.currentQuestion++;
     game.loadQuestion();
   },
+
+  // time up message
   timeUp() {
     clearInterval(timer);
-    $('#count-number').html(game.count);
+    $('#timed-number').html(game.count);
 
     panel.html('<h2>The Timer Ran Out!</h2>');
     panel.append(
-      `<h3>The Answer Is: ${questions[this.currentQuestion].correctAnswer}`
+      `<h3>The Answer Is: ${questions[this.currentQuestion].correctAnswer}`,
+      `<img src="${questions[this.currentQuestion].image}"/>`,
     );
 
     if (game.currentQuestion === questions.length - 1) {
@@ -165,19 +175,8 @@ const game = {
       setTimeout(game.nextQuestion, 3000);
     }
   },
-  results() {
-    clearInterval(timer);
 
-    panel.html('<h2>Good Job! Lets See How You Did.</h2>');
-    $('#count-number').html(game.count);
-    panel.append(`<h3>Answered Correctly: ${game.correct}</h3>`);
-    panel.append(`<h3>Answered Incorrectly: ${game.incorrect}</h3>`);
-    panel.append(
-      `<h3>Left Unanswered: ${questions.length -
-        (game.incorrect + game.correct)}</h3>`
-    );
-    panel.append('<br><button class="start-over">Play Again</button>');
-  },
+  // calling if answer right or wrong
   clicked(e) {
     clearInterval(timer);
 
@@ -189,13 +188,19 @@ const game = {
       this.answeredIncorrectly();
     }
   },
+
+  // incorrect message
   answeredIncorrectly() {
     game.incorrect++;
     clearInterval(timer);
     panel.html('<h2>Ah Man! That Wasnt Right!</h2>');
     panel.append(
-      `<h3>The Answer Is: ${questions[game.currentQuestion].correctAnswer}</h3>`
-    );
+      `<h3>The Answer Is: ${
+        questions[game.currentQuestion].correctAnswer
+      }</h3>`,
+      `<img src="${questions[this.currentQuestion].image}"/>`,
+    ),
+      `<img src="${questions[this.currentQuestion].image}"/>`;
 
     if (game.currentQuestion === questions.length - 1) {
       setTimeout(game.results, 3000);
@@ -203,16 +208,34 @@ const game = {
       setTimeout(game.nextQuestion, 3000);
     }
   },
+
+  // correct message
   answeredCorrectly() {
     clearInterval(timer);
     game.correct++;
-    panel.html('<h2>Yay! You Got It Right!</h2>');
+    panel.html('<h2>Yay! You Got It Right!</h2>'),
+      panel.append(`<img src="${questions[this.currentQuestion].image}"/>`);
 
     if (game.currentQuestion === questions.length - 1) {
       setTimeout(game.results, 3000);
     } else {
       setTimeout(game.nextQuestion, 3000);
     }
+  },
+
+  // result page message
+  results() {
+    clearInterval(timer);
+
+    panel.html('<h2>Good Job! Lets See How You Did.</h2>');
+    $('#timed-number').html(game.count);
+    panel.append(`<h3>Answered Correctly: ${game.correct}</h3>`);
+    panel.append(`<h3>Answered Incorrectly: ${game.incorrect}</h3>`);
+    panel.append(
+      `<h3>Left Unanswered: ${questions.length -
+        (game.incorrect + game.correct)}</h3>`,
+    );
+    panel.append('<br><button class="play-again">Play Again</button>');
   },
   reset() {
     this.currentQuestion = 0;
